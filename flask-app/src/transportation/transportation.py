82d5d4219d2a9dcd8c2d5d42529ebb15fbd0eb65
vdema <demaisip.v@northeastern.edu>
2024-04-16 18:25:55 -0400

    Relocated the GET route for the room_types from transportation to reservations

--- flask-app/src/transportation/transportation.py
+++ flask-app/src/transportation/transportation.py
@@ -104,29 +104,3 @@ def get_navigation_routes():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
-
-# Gets all the room types, their info, and the hotels with the room type from the database
-@transportation.route('/room_type', methods=['GET'])
-def get_room_types():
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-
-    # use cursor to query the database for a list of room types
-    cursor.execute('SELECT type_id, Name, max_guests, total_room, description, room_price, hotel_id FROM room_type')
-
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in
-    # putting column headers together with data
-    json_data = []
-
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers.
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
